b0637362cd18dafe57076b22ca76747e1c832c13
Stefan Veit <Stefan.Veit@dlr.de>
2021-04-10 00:23:46 +0200

    created

---
+++ backend/entity.cpp
@@ -0,0 +1,143 @@
+#include "entity.h"
+
+// Entity methods
+
+Entity::Entity(std::string a_name, int a_zone, int a_pos_x, int a_pos_y, int a_strength, int a_dexterity, int a_intelligence, int a_health, int a_give_xp)
+{
+    name = a_name;
+    zone = a_zone;
+    position_x = a_pos_x;
+    position_y = a_pos_y;
+        
+    hp_current = a_health;
+    hp_max = a_health;
+    health = calculate_health();
+        
+    strength = a_strength;
+    dexterity = a_dexterity;
+    intelligence = a_intelligence;
+        
+    give_xp = a_give_xp;
+     
+}
+
+bool Entity::is_attacked(int a_attack, int a_damage)
+{
+    bool hit = false;
+    if (a_attack >= 10 + calculate_armor()) { hit = true; }
+    if (hit) { hp_current -= a_damage; }
+    return hit;
+}
+    
+void Entity::heal(int a_boost_hp)
+{
+    hp_current += a_boost_hp;
+    if (hp_current > hp_max) { hp_current = hp_max; }
+}
+    
+
+// Player methods
+
+
+Player::Player(std::string a_name, int a_zone, int a_pos_x, int a_pos_y) : Entity(a_name, a_zone, a_pos_x, a_pos_y, 0, 0, 0, 4, 10) 
+{
+    level = 0;
+    experience = 0;
+    next_experience = 0;
+    give_attribute_points = 0;
+}
+
+void Player::store(void)
+{
+    cape::Udc udc_player(CAPE_UDC_NODE);
+    udc_player.add("name", name);
+    udc_player.add("zone", zone);
+    udc_player.add("position_x", position_x);
+    udc_player.add("position_y", position_y);
+    udc_player.add("hp_current", hp_current);
+    udc_player.add("hp_max",     hp_max);
+    udc_player.add("strength",   strength);
+    udc_player.add("dexterity",  dexterity);
+    udc_player.add("intelligence", intelligence);
+    udc_player.add("experience", experience);
+    udc_player.add("give_attribute_points", give_attribute_points);
+
+    std::string to_file = udc_player.to_string();
+    std::ofstream MyFile("save_players/" + name + ".txt");
+    MyFile << to_file;
+    MyFile.close();
+}
+
+void Player::load(void)
+{
+    CapeErr err;
+    std::string filename = "save_players/" + name + ".txt";
+        
+    // check existance of savegame
+    std::ifstream myFile;
+    myFile.open(filename.c_str());
+    if(myFile) 
+    {
+            
+        myFile.close();
+        printf("loading player from %s ... ", filename.c_str()); 
+                       
+        cape::Udc content = cape_json_from_file (filename.c_str(), err);
+          
+        if (content.get("position_x").valid())  { position_x    = content.get("position_x"); }
+        if (content.get("position_y").valid())  { position_y    = content.get("position_y"); }
+        if (content.get("zone").valid())        { zone          = content.get("zone"); }
+        if (content.get("hp_current").valid())  { hp_current    = content.get("hp_current"); }
+        if (content.get("hp_max").valid())      { hp_max        = content.get("hp_max"); }
+        if (content.get("strength").valid())    { strength      = content.get("strength"); }
+        if (content.get("dexterity").valid())   { dexterity     = content.get("dexterity"); }
+        if (content.get("intelligence").valid()) { intelligence = content.get("intelligence"); }
+        if (content.get("experience").valid())  { experience = content.get("experience"); }
+        if (content.get("give_attribute_points").valid())  { give_attribute_points = content.get("give_attribute_points"); }
+            
+        calculate_current_level();
+        calculate_next_level();
+            
+    } else { printf("File does not exist yet!\n"); }
+}
+   
+void Player::give_attribute(int a_attribute)
+{
+    if (give_attribute_points > 0)
+    {
+        if (0 == a_attribute) { strength++; }
+        else if (1 == a_attribute) { dexterity++; }
+        else { intelligence++; }
+            
+        give_attribute_points--;
+           
+        if (give_attribute_points == 0) // finalize level up
+        {
+            experience -= next_experience;
+            calculate_current_level();
+            calculate_next_level();  
+        }
+    }
+}
+        
+
+int Player::boost_xp(int a_boost) 
+{
+    
+    calculate_current_level();
+    calculate_next_level();
+
+    experience += a_boost;
+        
+    // level up
+    if (experience >= next_experience)
+    {
+        give_attribute_points += 1;            
+        hp_max += rand() % (strength + 1) + 1;
+        hp_current = hp_max;
+            
+    }
+}
+
+   
+
---
+++ backend/entity.h
@@ -0,0 +1,122 @@
+#ifndef _ENTITY_H_
+#define _ENTITY_H_
+
+#include <iostream>
+#include <fstream> 
+#include <string>
+
+#include <fmt/cape_json.h>
+#include <hpp/cape_stc.hpp>
+
+
+#define VOLUME_SECTOR_MAX_X 1000
+#define VOLUME_SECTOR_MAX_Y 1000
+
+class Entity
+{
+    
+public:     // public members
+    
+    Entity(std::string a_name, int a_zone, int a_pos_x, int a_pos_y, int a_strength, int a_dexterity, int a_intelligence, int a_health, int a_give_xp);
+    
+    std::string get_name() { return name; }
+    int get_zone() { return zone; }
+    int get_position_x() { return position_x; }
+    int get_position_y() { return position_y; }
+
+    int get_hp_current() { return hp_current; }
+    int get_hp_max() { return hp_max; }
+    int calculate_health() { health = hp_current * 100 / hp_max; return health; }
+    bool is_dead() { return hp_current <= 0; }
+    
+    int get_strength() { return strength; }
+    int get_dexterity() { return dexterity; }
+    int get_intelligence() { return intelligence; }
+    
+    int calculate_armor() { return 0; } // regard items
+    int calculate_weapon() { return 1; } // regard items
+    int create_attack() { return rand() % 20 + 1 + get_dexterity(); }
+    int create_damage() { int damage = rand() * calculate_weapon() / RAND_MAX + 1 + get_strength(); if (damage < 0) { damage = 0; } return damage; }
+    
+    void set_position(int a_x, int a_y) { position_x = a_x; position_y = a_y; }
+    void move_right() { position_x++; if (position_x > VOLUME_SECTOR_MAX_X)  { position_x = VOLUME_SECTOR_MAX_X; } }
+    void move_left()  { position_x--; if (position_x < 0)                    { position_x = 0; } }
+    void move_up()    { position_y++; if (position_y > VOLUME_SECTOR_MAX_Y)  { position_y = VOLUME_SECTOR_MAX_Y; } }
+    void move_down()  { position_y--; if (position_y < 0)                    { position_y = 0; } }
+    
+    
+    bool is_attacked(int a_attack, int a_damage);    
+    void heal(int a_boost_hp);
+    
+    int get_give_xp() { return give_xp; }
+
+    
+protected:    // private attributes
+    std::string name;
+    int zone;
+    int position_x;
+    int position_y;
+    int health;
+    
+    // attributes
+    int strength, dexterity, intelligence;
+    
+    // health
+    int hp_current, hp_max; // health is in percent
+    
+    // experience
+    int give_xp; // XP to enemy when defeated
+    
+};
+
+class Monster : public Entity
+{
+private:
+    int m_view_x;
+    int m_view_y;
+    
+public:
+    Monster(std::string a_name, int a_zone, int a_pos_x, int a_pos_y, int a_view_x, int a_view_y, int a_strength, int a_dexterity, int a_intelligence, int a_health, int a_give_xp) : 
+        Entity(a_name, a_zone, a_pos_x, a_pos_y, a_strength, a_dexterity, a_intelligence, a_health, a_give_xp) 
+        {
+            m_view_x = a_view_x; if (m_view_x < 0) { m_view_x = 0; }
+            m_view_y = a_view_y; if (m_view_y < 0) { m_view_y = 0; }
+        }
+    int get_view_x() { return m_view_x; }
+    int get_view_y() { return m_view_y; }
+    
+};
+
+class Player : public Entity
+{
+    
+public:
+    Player(std::string a_name, int a_zone, int a_pos_x, int a_pos_y);
+    int calculate_current_level() { level = strength + dexterity + intelligence; return level; }
+    int calculate_next_level() { next_experience = (level+1) * 30; return next_experience; }
+               
+    void store(void);
+    void load(void);
+    
+    int get_level() { return level; }
+    int get_experience() { return experience; }
+    int get_next_experience() { return next_experience; }
+    int get_give_attribute_points() { return give_attribute_points; }
+    
+    void give_strength() { give_attribute(0); }
+    void give_dexterity() { give_attribute(1); }
+    void give_intelligence() { give_attribute(2); }
+    
+    void give_attribute(int a_attribute);
+    int boost_xp(int a_boost);
+
+private:
+    
+    // === attributes ===
+    
+    // experience
+    int level, experience, next_experience, give_attribute_points;
+    
+};
+
+#endif
